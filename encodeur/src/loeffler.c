6b228baf5f86e9e9d7f5a9992a430e3e37d4db5e
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2015-06-05 04:06:24 +0200

    Ajout loeffler DCT : encodeur très rapide !

--- encodeur/src/loeffler.c
+++ encodeur/src/loeffler.c
@@ -4,13 +4,22 @@
 #include "library.h"
 
 
-static inline void rotation(double i0, double i1, double *out0, double *out1, uint8_t k, uint8_t n)
+static inline void rotation_idct(double i0, double i1, double *out0, double *out1, uint8_t k, uint8_t n)
 {
         const double COS = cos((n * M_PI)/16);
         const double SIN = sin((n * M_PI)/16);
 
         *out0 = k * (i0 * COS - i1 * SIN);
         *out1 = k * (i1 * COS + i0 * SIN);
+}
+
+static inline void rotation_dct(double i0, double i1, double *out0, double *out1, uint8_t k, uint8_t n)
+{
+        const double COS = cos((n * M_PI)/16);
+        const double SIN = sin((n * M_PI)/16);
+
+        *out0 = k * (i0 * COS + i1 * SIN);
+        *out1 = k * (i1 * COS - i0 * SIN);
 }
 
 static inline void swap(double **a, double **b)
@@ -27,7 +36,7 @@ static inline void butterfly(double i0, double i1, double *out0, double *out1)
         *out1 = i0 - i1;
 }
 
-static inline void loeffler(double vect[BLOCK_DIM])
+static inline void loeffler_idct(double vect[BLOCK_DIM])
 {
         double next_buf[BLOCK_DIM];
         double *next = next_buf;
@@ -54,7 +63,7 @@ static inline void loeffler(double vect[BLOCK_DIM])
         butterfly(vect[4], vect[6], &next[4], &next[6]);
         butterfly(vect[7], vect[5], &next[7], &next[5]);
 
-        rotation(vect[2], vect[3], &next[2], &next[3], M_SQRT2, 6);
+        rotation_idct(vect[2], vect[3], &next[2], &next[3], M_SQRT2, 6);
 
         swap(&vect, &next);
 
@@ -64,8 +73,8 @@ static inline void loeffler(double vect[BLOCK_DIM])
         butterfly(vect[0], vect[3], &next[0], &next[3]);
         butterfly(vect[1], vect[2], &next[1], &next[2]);
 
-        rotation(vect[4], vect[7], &next[4], &next[7], 1, 3);
-        rotation(vect[5], vect[6], &next[5], &next[6], 1, 1);
+        rotation_idct(vect[4], vect[7], &next[4], &next[7], 1, 3);
+        rotation_idct(vect[5], vect[6], &next[5], &next[6], 1, 1);
 
         swap(&vect, &next);
 
@@ -92,7 +101,7 @@ void idct_block(int32_t in[64], uint8_t out[64])
                 for (uint8_t y = 0; y < BLOCK_DIM; ++y)
                         vector[y] = in[x*BLOCK_DIM + y];
 
-                loeffler(vector);
+                loeffler_idct(vector);
 
                 for (uint8_t y = 0; y < BLOCK_DIM; ++y)
                         matrix[x*BLOCK_DIM + y] = vector[y];
@@ -104,7 +113,7 @@ void idct_block(int32_t in[64], uint8_t out[64])
                 for (uint8_t x = 0; x < BLOCK_DIM; ++x)
                         vector[x] = matrix[x*BLOCK_DIM + y];
 
-                loeffler(vector);
+                loeffler_idct(vector);
 
                 /* Avec Loeffler on multiplie par sqrt(2) donc
                  * après chaque application il faut diviser par
@@ -115,37 +124,92 @@ void idct_block(int32_t in[64], uint8_t out[64])
         }
 }
 
-double C(const uint8_t x)
+
+static inline void loeffler_dct(double vect[BLOCK_DIM])
 {
-        double res = 1;
+        double next_buf[BLOCK_DIM];
+        double *next = next_buf;
 
-        if (x == 0)
-                res = M_SQRT1_2;
 
-        return res;
-}
+        /* Etape 1 */
 
-double S_dct(const uint8_t i, const uint8_t j, uint8_t *in)
-{
-        double sum = 0;
+        butterfly(vect[0], vect[7], &next[0], &next[7]);
+        butterfly(vect[1], vect[6], &next[1], &next[6]);
+        butterfly(vect[2], vect[5], &next[2], &next[5]);
+        butterfly(vect[3], vect[4], &next[3], &next[4]);
 
-        for (uint8_t x = 0; x < BLOCK_DIM; x++) {
-                for (uint8_t y = 0; y < BLOCK_DIM; y++) {
-                        sum +=  cos(((2*x + 1) * i * M_PI)/ (2*BLOCK_DIM)) *
-                                cos(((2*y + 1) * j * M_PI)/ (2*BLOCK_DIM)) *
-                                (in[x*BLOCK_DIM + y]-128);
-                }
-        }
+        swap(&vect, &next);
 
-        return sum;
+
+        /* Etape 2 */
+
+        butterfly(vect[0], vect[3], &next[0], &next[3]);
+        butterfly(vect[1], vect[2], &next[1], &next[2]);
+
+        rotation_dct(vect[4], vect[7], &next[4], &next[7], 1, 3);
+        rotation_dct(vect[5], vect[6], &next[5], &next[6], 1, 1);
+
+        swap(&vect, &next);
+
+
+        /* Etape 3 */
+
+        butterfly(vect[0], vect[1], &next[0], &next[1]);
+        butterfly(vect[4], vect[6], &next[4], &next[6]);
+        butterfly(vect[7], vect[5], &next[7], &next[5]);
+
+        rotation_dct(vect[2], vect[3], &next[2], &next[3], M_SQRT2, 6);
+
+        swap(&vect, &next);
+
+
+        /* Etape 4 */
+
+        next[0] = vect[0];
+        next[4] = vect[1];
+        next[2] = vect[2];
+        next[6] = vect[3];
+
+        butterfly(vect[7], vect[4], &next[1], &next[7]);
+
+        next[3] = M_SQRT2 * vect[5];
+        next[5] = M_SQRT2 * vect[6];
+
+        /* Inutile, remet la copie locale du pointeur vect à la bonne adresse */
+        //swap(&vect, &next);
 }
 
 void dct_block(uint8_t in[64], int32_t out[64])
 {
-        for (uint8_t i = 0; i < BLOCK_DIM; i++) {
-                for (uint8_t j = 0; j < BLOCK_DIM; j++) {
-                        out[i*BLOCK_DIM + j] = (int32_t)(C(i)*C(j)*S_dct(i, j, in)/4);
-                }
+        double vector[BLOCK_DIM];
+        double matrix[BLOCK_SIZE];
+
+        /* Application sur les lignes de la matrice */
+        for (uint8_t x = 0; x < BLOCK_DIM; ++x) {
+
+                for (uint8_t y = 0; y < BLOCK_DIM; ++y)
+                        vector[y] = (in[x*BLOCK_DIM + y] - 128.);
+
+                loeffler_dct(vector);
+
+                for (uint8_t y = 0; y < BLOCK_DIM; ++y)
+                        matrix[x*BLOCK_DIM + y] = vector[y];
+        }
+
+        /* Application sur les lignes de la transposée */
+        for (uint8_t y = 0; y < BLOCK_DIM; ++y) {
+
+                for (uint8_t x = 0; x < BLOCK_DIM; ++x)
+                        vector[x] = matrix[x*BLOCK_DIM + y];
+
+                loeffler_dct(vector);
+
+                /* Avec Loeffler on multiplie par sqrt(2) donc
+                 * après chaque application il faut diviser par
+                 * sqrt(BLOCK_DIM) au lieu de sqrt(2 * BLOCK_DIM), et donc vu qu'on
+                 * l'applique 2 fois içi il faut diviser par sqrt(BLOCK_DIM)^2 = BLOCK_DIM */
+                for (uint8_t x = 0; x < BLOCK_DIM; ++x)
+                        out[x*BLOCK_DIM + y] = (int32_t)(vector[x] / BLOCK_DIM);
         }
 }
 
